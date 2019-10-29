24ffa390345283ac4e73e2738161c5411ef733e0
cyklop123 <irekbiela@gmail.com>
2019-10-29 16:07:39 +0100

    Add modules and compilation to bin/

--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package icalc;
 
 import java.util.Scanner;
+import icalc.*;
 
-/**
- *
- * @author dstudent
- */
-public class main {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //pobieranie danych
         Scanner s = new Scanner(System.in);
